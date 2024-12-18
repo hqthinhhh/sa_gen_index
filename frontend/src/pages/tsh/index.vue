1dbff1c49d5ed09c37ff848d53f326b679f6673e
Thanhha2571 <letranthanhha25072001@gmail.com>
2024-12-18 16:22:01 +0700

    update data

--- frontend/src/pages/tsh/index.vue
+++ frontend/src/pages/tsh/index.vue
@@ -12,15 +12,20 @@ div.pa-2
     p.font-weight-bold Số chủ đạo (Đường đời): {{chiSoChuDao}}
     span(v-html="soChuDaoContent")
     p.font-weight-bold Sứ mệnh {{chiSoSuMenh}}
+    span(v-html="chiSoSuMenhContent")
     p.font-weight-bold K/n đường đời sứ mệnh
     p.font-weight-bold Linh hồn {{chiSoTamHon}}
+    span(v-html="chiSoTamHonContent")
     p.font-weight-bold Nhân cách {{chiSoTinhCach}}
-    p.font-weight-bold Thái độ: {{chiSoThaiDo}}
+    span(v-html="chiSoTinhCachContent")
+    p.font-weight-bold Thái độ: {{soThaiDo}}
+    span(v-html="chiSoThaiDoContent")
     p.font-weight-bold Đam mê
     p.font-weight-bold Tư duy trải nghiệm
     p.font-weight-bold Tư duy Cảm xúc
     p.font-weight-bold Tư duy trực giác
     p.font-weight-bold Ngày sinh: {{chiSoNgaySinh}}
+    span(v-html="soNgaySinhContent")
     p.font-weight-bold Cân bằng: {{chiSoCanBang}}
     p.font-weight-bold Trưởng thành {{chiSoTruongThanh}}
     p.font-weight-bold Tháng cá nhân
@@ -35,7 +40,11 @@ div.pa-2
     import {dayjs} from "@/plugins"
     import {calculateNumerologyByDate, calculateNumerologyByName, reduceToSingleDigit} from "@/utils/calculateTsh.js";
     import ChiSoDuongDoi from "@/utils/tsh/index.js";
-
+    import ChiSoThaiDo from "@/utils/tsh/ChiSoThaiDo.js";
+    import SoNangLucNgaySinh from "@/utils/tsh/SoNangLucNgaySinh.js";
+    import ChiSoLinhHon from "@/utils/tsh/ChiSoLinhHon.js";
+    import ChiSoTinhCach from "@/utils/tsh/ChiSoTinhCach.js";
+    import ChiSoSuMenh from "@/utils/tsh/ChiSoSuMenh.js";
     const Test = defineComponent({
         name: 'Test',
         components: {},
@@ -69,20 +78,35 @@ div.pa-2
             const chiSoNgaySinh = computed(()=>{
                 return chiSoTheoSinhNhat.value ? chiSoTheoSinhNhat.value["chiSoNangLucNgaySinh"] : null
             })
-            const chiSoThaiDo = computed(()=>{
+            const soNgaySinhContent = computed(() => {
+                return SoNangLucNgaySinh[chiSoNgaySinh.value]?.replace(/\n/g, '<br>')
+            })
+            const soThaiDo = computed(()=>{
                 return chiSoTheoSinhNhat.value ? chiSoTheoSinhNhat.value["chiSoThaiDo"] : null
+            })
+            const chiSoThaiDoContent = computed(() => {
+                return ChiSoThaiDo[soThaiDo.value]?.replace(/\n/g, '<br>')
             })
             const chiSoTinhCach = computed(()=>{
                 return chiSoTheoTen.value ? chiSoTheoTen.value["chiSoTinhCach"] : null
             })
+            const chiSoTinhCachContent = computed(() => {
+                return ChiSoTinhCach[chiSoTinhCach.value]?.replace(/\n/g, '<br>')
+            })
             const chiSoTamHon = computed(()=>{
                 return chiSoTheoTen.value ? chiSoTheoTen.value["chiSoTamHon"] : null
+            })
+            const chiSoTamHonContent = computed(() => {
+                return ChiSoLinhHon[chiSoTamHon.value]?.replace(/\n/g, '<br>')
             })
             const chiSoCanBang = computed(()=>{
                 return chiSoTheoTen.value ? chiSoTheoTen.value["chiSoCanBang"] : null
             })
             const chiSoSuMenh = computed(()=>{
                 return chiSoTheoTen.value ? chiSoTheoTen.value["chiSoSuMenh"] : null
+            })
+            const chiSoSuMenhContent = computed(()=>{
+                return ChiSoSuMenh[chiSoSuMenh.value]?.replace(/\n/g, '<br>')
             })
             const chiSoTruongThanh = computed(()=>{
                 return reduceToSingleDigit(chiSoChuDao.value + chiSoSuMenh.value)
@@ -95,8 +119,13 @@ div.pa-2
                 resultTitle,
                 chiSoChuDao,
                 chiSoNgaySinh,
-                chiSoThaiDo,
+                soThaiDo,
                 soChuDaoContent,
+                chiSoThaiDoContent,
+                soNgaySinhContent,
+                chiSoTamHonContent,
+                chiSoTinhCachContent,
+                chiSoSuMenhContent,
                 onClickCalName,
                 chiSoTinhCach,
                 chiSoTamHon,
--- frontend/src/utils/tsh/index.js
+++ frontend/src/utils/tsh/index.js
@@ -1,3 +1,2 @@
 import ChiSoDuongDoi from "@/utils/tsh/ChiSoDuongDoi.js";
-
 export default ChiSoDuongDoi
